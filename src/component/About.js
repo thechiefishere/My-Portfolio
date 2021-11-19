1a9bc161b3b35bc240883ae358f82fb51f2df03c
John Toriola <john.toriola.tj@gmail.com>
2021-11-19 09:22:31 +0100

    Updated my info

--- src/component/About.js
+++ src/component/About.js
@@ -19,16 +19,15 @@ const About = () => {
           <div className="aside-details bio-aside-details">
             <h3 className="aside-title bio-aside-details-title">Info</h3>
             <p>
-              My name is John Toriola, I am a self-taught programmer from
-              Nigeria. I presently reside in Lagos, Nigeria. I have been
-              learning programming for over two years. I am competent in Object
-              Oriented Programming(OOP) and functional programming. I began my
-              foray into programming with Java and from there I ventured into
-              Web Programming with in-depth understanding of HTML, CSS,
-              Javascript, React and NodeJs. I am a regular on HackerEarth, and I
-              have completed a lot of challenges with varying difficulty levels.
-              I am a fast learner and I have very solid knowledge of algorithms
-              and Data Structures.
+              My name is John Toriola, am a self-taught programmer from Nigeria.
+              I presently reside in Lagos, Nigeria. I've been learning
+              programming for over two years. Am competent in Object Oriented
+              Programming(OOP) and functional programming. Started my foray into
+              programming with Java and from there ventured into Web Programming
+              with in-depth understanding of HTML, CSS, Javascript, React and
+              NodeJs. Am a regular on HackerEarth, and have completed a lot of
+              challenges with varying difficulty levels. Am a fast learner and
+              have very solid knowledge of algorithms and Data Structures.
             </p>
           </div>
         </aside>
@@ -45,8 +44,8 @@ const About = () => {
                 University and the other is "Object Oriented Java Programming -
                 Data Structure And Beyond" by UCSD. By reason of the courses, I
                 know how to use Data Structures such LinkedList, ArrayList,
-                Binary Search Trees(BST) and Graphs. And I can perform the
-                asymptotic analysis of an algorithm. I have also built some
+                Binary Search Trees(BST) and Graphs. And can perform the
+                asymptotic analysis of an algorithm. I've also built some
                 Android games using Java.
               </p>
             </div>
@@ -58,8 +57,8 @@ const About = () => {
                 As a front-end web developer, I can design web sites and apps
                 using HTML, CSS, Javascript and React. Apart from the
                 afforementioned tools, I can also build websites using bootstrap
-                and Sass. I completed two specializations on Coursera in other
-                to become a better web developer. The first is "Web Design for
+                and Sass. Completed two specializations on Coursera in other to
+                become a better web developer. The first is "Web Design for
                 Everybody" by University of Michigan and the second is
                 "Javascript for Beginners" by UCDavis.
               </p>
@@ -69,14 +68,14 @@ const About = () => {
             <div className="aside-details">
               <h3 className="aside-title">Back-End Web Developer</h3>
               <p>
-                I have completed two specializations on Cousera to solidify my
-                skill as a back-end web developer. The first is "Web Application
-                For Everybody" by the University of Michigan and the second is
-                "Full Stack Web Development With React" by Hong Kong University
-                of Science and Technology. From the two courses I was able to
-                learn how to use PHP and NodeJs to build a secure backend
-                application and REST Apis. Some fullstack websites I built are
-                in the Projects section.
+                Completed two specializations on Cousera to solidify my skill as
+                a back-end web developer. The first is "Web Application For
+                Everybody" by the University of Michigan and the second is "Full
+                Stack Web Development With React" by Hong Kong University of
+                Science and Technology. From the two courses I was able to learn
+                how to use PHP and NodeJs to build a secure backend application
+                and REST Apis. Some fullstack websites I built are in the
+                Projects section.
               </p>
             </div>
           </aside>
